0e6519567ee61512e9caaa5dc052a1525c2fcf69
Kirill Chugunov <chugunov@ferrata.pro>
2023-03-01 12:03:32 +0300

    fixes can be better after 1st review

--- javascript.js
+++ javascript.js
@@ -1,55 +1,62 @@
 const popuprofile = document.querySelector(".popup__profile-edit");
 const buttoneditProfile = document.querySelector(".profile__edit-button");
-const editProfileclosebutton = document.querySelector(".popup__close_edit-profile");
+const editProfileclosebutton = document.querySelector(
+  ".popup__close_edit-profile"
+);
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const popupFormTitle = document.querySelector(".form__field_profile_title");
-const popupFormSubtitle = document.querySelector(".form__field_profile_subtitle");
+const popupFormSubtitle = document.querySelector(
+  ".form__field_profile_subtitle"
+);
 const buttonAddprofile = document.querySelector(".profile__add-button");
 const popupelement = document.querySelector(".popup__add-element");
-const popupeditclosebutton = document.querySelector (".popup__close_edit-profile");
-const popupelementclosebutton = document.querySelector (".popup__close_add-element");
+const popupeditclosebutton = document.querySelector(
+  ".popup__close_edit-profile"
+);
+const popupelementclosebutton = document.querySelector(
+  ".popup__close_add-element"
+);
 const popupEditProfileForm = document.querySelector(".profile_popup_form");
 const popupBigImg = document.querySelector(".img-popup");
 
 ////////////////////Функция открытия попапа////////////////////
 function openPopup(popup) {
   popup.classList.add("popup_opened");
-};
+}
 ////////////////////Функция закрытия попапа/////////////////////////
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
-};
-
+}
 
 /////////////////////ВЫЗОВ ПЕРВОГО ПОПАПА////////////////////////////
-buttoneditProfile.addEventListener("click", function(){
+buttoneditProfile.addEventListener("click", function () {
   openPopup(popuprofile);
   popupFormTitle.value = profileTitle.textContent;
   popupFormSubtitle.value = profileSubtitle.textContent;
 });
 /////////////////////////ВЫЗОВ ВТОРОГО ПОПАПА//////////////////////////
-buttonAddprofile.addEventListener("click", function(){
+buttonAddprofile.addEventListener("click", function () {
   openPopup(popupelement);
-})
+});
 /////////////////////////ЗАКРЫТИЕ ПЕРВОГО ПОПАПА/////////////////////////
-popupeditclosebutton.addEventListener("click",function() {
+popupeditclosebutton.addEventListener("click", function () {
   closePopup(popuprofile);
-})
+});
 
 /////////////////////////ЗАКРЫТИЕ ВТОРОГО ПОПАПА/////////////////////////
-popupelementclosebutton.addEventListener("click",function() {
+popupelementclosebutton.addEventListener("click", function () {
   closePopup(popupelement);
-})
+});
 //////////////////////////САМБИТ ПЕРВОГО ПОПАПА////////////////////////////
-function handleSubmitTitleForm (e) {
+function handleSubmitTitleForm(e) {
   e.preventDefault();
   profileTitle.textContent = popupFormTitle.value;
   profileSubtitle.textContent = popupFormSubtitle.value;
   closePopup(popuprofile);
 }
 
-popupEditProfileForm.addEventListener("submit",handleSubmitTitleForm);
+popupEditProfileForm.addEventListener("submit", handleSubmitTitleForm);
 
 //////////////////ДОБАВЛЕНИЕ ИЗОБРАЖЕНИЙ/////////////////////////
 
@@ -82,7 +89,6 @@ const initialCards = [
   },
 ];
 
-
 // Нашли секцию Elements:
 const sectionElements = document.querySelector(".elements");
 // Нашли заготовку:
@@ -90,7 +96,9 @@ const elementsTemplate = document.querySelector("#element__template").content;
 //Создаем функцию для создания и возвращения карточки
 function buildcard(element) {
   //Клонировали заготовку
-  const placeElement = elementsTemplate.querySelector(".elements__card").cloneNode(true);
+  const placeElement = elementsTemplate
+    .querySelector(".elements__card")
+    .cloneNode(true);
   //Заполнили название фото
   placeElement.querySelector(".elements__caption").textContent = element.name;
   //Нашли фото в карточке:
@@ -99,35 +107,37 @@ function buildcard(element) {
   placeimg.src = element.link;
   placeimg.alt = placeElement.textContent;
   //Повесили слушателя на изображение на открытие попапа
-  placeimg.addEventListener("click",function(){
-   console.log("i am alive2")
-   openPopup(popupBigImg);
-   const bigimg = document.querySelector(".img-popup__figure");
-   bigimg.src = placeimg.src;
-   const bigcaption = document.querySelector(".img-popup__caption");
-   bigcaption.textContent = element.name;  
+  placeimg.addEventListener("click", function () {
+    console.log("i am alive2");
+    openPopup(popupBigImg);
+    const bigimg = document.querySelector(".img-popup__figure");
+    bigimg.src = placeimg.src;
+    const bigcaption = document.querySelector(".img-popup__caption");
+    bigcaption.textContent = element.name;
   });
-   // Нашли кнопку удаления
-   const imgdeletebutton = placeElement.querySelector(".elements__delete-button");
-   //Повесили слушателя на удаление:
-   imgdeletebutton.addEventListener('click', function () {
-     const carditem = imgdeletebutton.closest('.elements__card');
-     carditem.remove();
-   }); 
-   //Нашли кнопку лайка 
-   const likebutton = placeElement.querySelector(".elements__heart-button");
-   //Повесили слушателя на лайк:
-   likebutton.addEventListener('click', function () {
+  // Нашли кнопку удаления
+  const imgdeletebutton = placeElement.querySelector(
+    ".elements__delete-button"
+  );
+  //Повесили слушателя на удаление:
+  imgdeletebutton.addEventListener("click", function () {
+    const carditem = imgdeletebutton.closest(".elements__card");
+    carditem.remove();
+  });
+  //Нашли кнопку лайка
+  const likebutton = placeElement.querySelector(".elements__heart-button");
+  //Повесили слушателя на лайк:
+  likebutton.addEventListener("click", function () {
     likebutton.classList.toggle("elements__heart-button_active");
     console.log("i am alive");
-  }); 
+  });
   return placeElement;
 }
 
 const placeimgclosebutton = document.querySelector(".img-popup__close");
-placeimgclosebutton.addEventListener("click",function(){
+placeimgclosebutton.addEventListener("click", function () {
   closePopup(popupBigImg);
- });
+});
 
 //Функция добавления карты в верстку
 function createCard(element) {
@@ -137,41 +147,25 @@ function createCard(element) {
 
 //Добавляем карты из массива:
 function gridBuilder() {
-   initialCards.forEach(createCard);
+  initialCards.forEach(createCard);
 }
-gridBuilder()
+gridBuilder();
 //Работает!!!!!
-const addimgFormTitle = document.querySelector(".form__field_elements_title")
-const addCaptionFormTitle = document.querySelector(".form__field_elements_subtitle")
+const addimgFormTitle = document.querySelector(".form__field_elements_title");
+const addCaptionFormTitle = document.querySelector(
+  ".form__field_elements_subtitle"
+);
 
 function addNewCard(e) {
   e.preventDefault();
-    const card = {
+  const card = {
     name: addimgFormTitle.value,
     link: addCaptionFormTitle.value,
-  }
-  createCard(card)
-  console.log("I AM ALIVE!!!1")
+  };
+  createCard(card);
+  console.log("I AM ALIVE!!!1");
   closePopup(popupelement);
- }
-const addimgbutton = document.querySelector(".elemnt__submit")
-popupelement.addEventListener("submit",addNewCard);
-
-// Функция удаления карточки:
-  // Нашли кнопку удаления
-  // const imgdeletebutton = document.querySelector(".elements__delete-button");
-  // //Повесили слушателя на удаление:
-  // imgdeletebutton.addEventListener('click', function () {
-  //   const carditem = imgdeletebutton.closest('.elements__card');
-  //   carditem.remove();
-  // }); 
-
-
-
-
-
-
-
-
-
+}
+const addimgbutton = document.querySelector(".elemnt__submit");
+popupelement.addEventListener("submit", addNewCard);
 
