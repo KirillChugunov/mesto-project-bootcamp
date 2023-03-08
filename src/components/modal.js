06e1c6a4f81cda9142971cb7fe36182e3a58724e
Kirill Chugunov <chugunov@ferrata.pro>
2023-03-08 21:29:23 +0300

    webpack works

---
+++ src/components/modal.js
@@ -0,0 +1,42 @@
+import {
+  fullDocument,
+  ProfileTitle,
+  PopupFormTitle,
+  ProfileSubtitle,
+  PopupFormSubtitle,
+  popupEditProfile,
+} from "../index.js";
+////////////////////Функция открытия попапа////////////////////
+export function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  fullDocument.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+      closePopup(popup);
+    }
+  });
+  popup.addEventListener("click", function (evt) {
+    if (evt.target === popup) {
+      closePopup(popup);
+    }
+  });
+}
+
+////////////////////Функция закрытия попапа/////////////////////////
+export function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+  PopupFormTitle.value = "";
+  PopupFormSubtitle.value = "";
+  fullDocument.removeEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+      closePopup(popup);
+    }
+  });
+}
+
+//////////////////////////САМБИТ ПЕРВОГО ПОПАПА////////////////////////////
+export function handleSubmitTitleForm(e) {
+  e.preventDefault();
+  ProfileTitle.textContent = PopupFormTitle.value;
+  ProfileSubtitle.textContent = PopupFormSubtitle.value;
+  closePopup(popupEditProfile);
+}
